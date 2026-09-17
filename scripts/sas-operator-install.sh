77dd535dea2e9b8ad6d2441c6cafbe170110fcfa
arkaul <arvind.kaul@sas.com>
2026-09-17 16:25:50 +0200

    Init

--- scripts/sas-operator-install.sh
+++ scripts/sas-operator-install.sh
@@ -2,5 +2,3 @@
 cd ../deployment-operator/deploy/
 oc kustomize -o sasoperator.yaml
 oc apply -f sasoperator.yaml -n sasviya
-#oc new-project sasoperator
-#oc apply -f sasoperator.yaml -n sasoperator
